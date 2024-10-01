12af82fd5ee6403e92d997bc892f166eef6804fc
Frank J. Gómez <frank@pawpawworks.com>
2024-09-30 20:22:49 -0400

    Adapted example WS2812 sketch for Arduino Uno.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,52 @@
+// adapted from https://github.com/smart-leds-rs/smart-leds-samples/blob/master/avr-examples/examples/avr_ws2812_blink_spi_pre.rs,
+// targeting the Arduino Uno (via the generic HAL) rather than the Arduino Leonardo
+
 #![no_std]
 #![no_main]
 
 use panic_halt as _;
+
+use arduino_hal::spi;
+use smart_leds::{SmartLedsWrite, RGB8};
+use ws2812_spi::Ws2812;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let (spi, _) = spi::Spi::new(
+        dp.SPI,
+        pins.d13.into_output(),
+        pins.d11.into_output(),
+        pins.d12.into_pull_up_input(),
+        pins.d10.into_output(),
+        spi::Settings::default(),
+    );
 
-    let mut led = pins.d13.into_output();
+    let mut data: [RGB8; 3] = [RGB8::default(); 3];
+    let empty: [RGB8; 3] = [RGB8::default(); 3];
+    let mut ws = Ws2812::new(spi);
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
+        data[0] = RGB8 {
+            r: 0,
+            g: 0,
+            b: 0x10,
+        };
+        data[1] = RGB8 {
+            r: 0,
+            g: 0x10,
+            b: 0,
+        };
+        data[2] = RGB8 {
+            r: 0x10,
+            g: 0,
+            b: 0,
+        };
+        ws.write(data.iter().cloned()).unwrap();
+        arduino_hal::delay_ms(1000 as u16);
+        ws.write(empty.iter().cloned()).unwrap();
+        arduino_hal::delay_ms(1000 as u16);
     }
 }
