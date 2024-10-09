b1d6ca8fea3d3c125f09e49cd0280ee9379a1d3c
Frank J. Gómez <frank@pawpawworks.com>
2024-10-09 14:24:34 -0400

    Simultaneously display arrow and rudimentary random color assignment/glitchy blink patterns.
    The essential change in this patch is the use of ranges in the LED strip collection
    instead of slices of the same. The previous approach resulted in multiple mutable references
    to the same LED object (i.e., strip collection item) or immutable uses (in ws.write) with
    mutable references still in scope, blocking compilation. More than anything, the slices
    served to improve readability of the code (e.g., arrow_leds); to hold on to some of that
    legibility, constants have been created for ranges in the strip (e.g., strip[ARROW_LED_RANGE]).

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod effects;
 mod tick;
 
 use arduino_hal::spi;
+use core::ops::Range;
 use effects::{arrow::Arrow, color_set::ColorSet};
 use panic_halt as _;
 use rand::{rngs::SmallRng, Rng, SeedableRng};
@@ -13,9 +14,11 @@ use tick::Tick;
 use ws2812_spi::Ws2812;
 
 // global configurations
-const LED_CNT: usize = 21;
-// does not include the head of the running lights
-const TAIL_CNT: usize = 5;
+const ARROW_LED_CNT: usize = 21;
+const ARROW_LED_RANGE: Range<usize> = 0..ARROW_LED_CNT;
+const GLITCH_LED_CNT: usize = 5;
+const GLITCH_LED_RANGE: Range<usize> = ARROW_LED_CNT..(ARROW_LED_CNT + GLITCH_LED_CNT);
+const TAIL_CNT: usize = 5; // does not include the head of the running lights
 const TICKS_PER_SEC: u16 = 100;
 const YELLOW: RGB8 = RGB8 {
     r: 255,
@@ -27,6 +30,7 @@ const RED: RGB8 = RGB8 {
     g: 74,
     b: 65,
 };
+// end config
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -46,28 +50,32 @@ fn main() -> ! {
     let mut rng = SmallRng::seed_from_u64(361279846193);
 
     // define the strip with the LEDs initialized in the "off" setting
-    let mut strip = [RGB8::default(); LED_CNT];
+    let mut strip = [RGB8::default(); ARROW_LED_CNT + GLITCH_LED_CNT];
 
-    // let mut arrow = Arrow::new(&YELLOW, TAIL_CNT);
+    let mut arrow = Arrow::new(&YELLOW, TAIL_CNT);
 
     // randomly assign colors from our palette to the LEDs; this (and the clone)
     // happens outside the loop because we don't want the LEDs changing colors
     // once they've been assigned (and because we want to reassign the same color
     // to an LED after turning it off)
     let mut color_set = ColorSet::new(&[RED, YELLOW], &mut rng);
-    color_set.mutate(&mut strip);
-    let strip_init = strip.clone();
+    color_set.mutate(&mut strip[GLITCH_LED_RANGE]);
+    let mut glitch_init = [RGB8 { r: 0, g: 0, b: 255 }; GLITCH_LED_CNT];
+    glitch_init.clone_from_slice(&strip[GLITCH_LED_RANGE]);
+    // hack to freeze the array
+    let glitch_init = glitch_init;
 
     let mut tick = Tick::new(TICKS_PER_SEC);
     loop {
+        // glitchy flash effect
         if tick.elapsed() < 10 || (19 < tick.elapsed() && tick.elapsed() <= 30) {
-            strip = strip_init;
+            strip[GLITCH_LED_RANGE].clone_from_slice(&glitch_init);
         } else if 50 <= tick.elapsed() && tick.elapsed() < 90 {
             // the second half of a second, leave the lights on
-            strip = strip_init;
+            strip[GLITCH_LED_RANGE].clone_from_slice(&glitch_init);
 
             // ...with a 90% chance of glitching
-            for (i, led) in strip.iter_mut().enumerate() {
+            for (i, led) in strip[GLITCH_LED_RANGE].iter_mut().enumerate() {
                 let random = rng.gen_range(1..=10);
                 if i % 2 == 0 && random > 9 {
                     *led = RGB8::default();
@@ -75,11 +83,16 @@ fn main() -> ! {
             }
         } else {
             // turn off every other light
-            for (i, led) in strip.iter_mut().enumerate() {
+            for (i, led) in strip[GLITCH_LED_RANGE].iter_mut().enumerate() {
                 if i % 2 == 0 {
                     *led = RGB8::default();
                 }
             }
+        }
+
+        // arrow effect
+        if tick.elapsed() % 10 == 0 {
+            arrow.mutate(&mut strip[ARROW_LED_RANGE]);
         }
 
         ws.write(gamma(strip.iter().cloned())).unwrap();
