6ea4daeccdbcce26814b2afd8490964cc8b94c91
Frank J. Gómez <frank@pawpawworks.com>
2024-10-21 18:21:27 -0400

    Extend patterns to full LED strip and insert arrow pattern between glitching LEDs.
    Increasing the number of LEDs for which the Arduino is responsible greatly increased the
    amount of time required to complete one "loop." Therefore, the Tick module (i.e., time)
    as an organizing principle no longer made as much sense for organizing animations as did
    frames.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 #![no_std]
 #![no_main]
 
+mod animation;
 mod effects;
-mod tick;
 
+use animation::Animation;
 use arduino_hal::spi;
-use core::ops::Range;
+use core::cmp::Ordering;
+use core::ops::{RangeFrom, RangeInclusive, RangeTo};
 use effects::{arrow::Arrow, color_set::ColorSet};
 use panic_halt as _;
 use rand::{rngs::SmallRng, Rng, SeedableRng};
 use smart_leds::{gamma, SmartLedsWrite, RGB8};
-use tick::Tick;
 use ws2812_spi::Ws2812;
 
 // global configurations
-const ARROW_LED_CNT: usize = 21;
-const ARROW_LED_RANGE: Range<usize> = 0..ARROW_LED_CNT;
-const GLITCH_LED_CNT: usize = 5;
-const GLITCH_LED_RANGE: Range<usize> = ARROW_LED_CNT..(ARROW_LED_CNT + GLITCH_LED_CNT);
-const TAIL_CNT: usize = 5; // does not include the head of the running lights
-const TICKS_PER_SEC: u16 = 100;
+const ARROW_START: usize = 100;
+const ARROW_END: usize = 200;
+const GLITCH_FRAME_CNT: usize = 150;
+const LED_CNT: usize = 300;
+const TAIL_CNT: usize = 15; // does not include the head of the running lights
 const YELLOW: RGB8 = RGB8 {
     r: 255,
     g: 207,
@@ -30,6 +30,11 @@ const RED: RGB8 = RGB8 {
     g: 74,
     b: 65,
 };
+
+// somewhat derived configs...
+const ARROW_LED_RANGE: RangeInclusive<usize> = ARROW_START..=ARROW_END;
+const GLITCH_LED_RANGE_1: RangeTo<usize> = ..ARROW_START;
+const GLITCH_LED_RANGE_2: RangeFrom<usize> = (ARROW_END + 1)..;
 // end config
 
 #[arduino_hal::entry]
@@ -50,13 +55,13 @@ fn main() -> ! {
     let mut rng = SmallRng::seed_from_u64(361279846193);
 
     // define the strip with the LEDs initialized in the "off" setting
-    let mut strip = [RGB8::default(); ARROW_LED_CNT + GLITCH_LED_CNT];
+    let mut strip = [RGB8::default(); LED_CNT];
 
     let mut arrow = Arrow::new(&YELLOW, TAIL_CNT);
 
     let mut color_set = ColorSet::new(&[RED, YELLOW]);
 
-    let mut tick = Tick::new(TICKS_PER_SEC);
+    let mut animation = Animation::new(LED_CNT);
     loop {
         // Assign colors from our palette to the LEDs in a seemingly random but idempotent way.
         // This has to happen inside the loop (and hence can't be actually randomly assigned)
@@ -65,14 +70,31 @@ fn main() -> ! {
         // assures that, even though some LEDs will be turned off in some cycles, each LED will
         // have its original color assigned the next time through, as opposed to being lit red
         // one cycle, dark the next, and yellow after that.
-        color_set.mutate(&mut strip[GLITCH_LED_RANGE]);
+        color_set.mutate(&mut strip);
 
-        // glitchy flash effect
-        if tick.elapsed() < 10 || (19 < tick.elapsed() && tick.elapsed() <= 30) {
+        // glitchy flash effect -- this repeats after 150 frames (half as often as the overall animation);
+        // to keep the conditions below a little simpler provide an effect-specific frame counter
+        let glitch_frames_displayed = match animation.frames_displayed().cmp(&GLITCH_FRAME_CNT) {
+            Ordering::Less => animation.frames_displayed(),
+            Ordering::Equal => 0,
+            Ordering::Greater => animation
+                .frames_displayed()
+                .saturating_sub(GLITCH_FRAME_CNT),
+        };
+
+        if glitch_frames_displayed < 20
+            || (30 < glitch_frames_displayed && glitch_frames_displayed <= 45)
+        {
             // leave the lights on
-        } else if 50 <= tick.elapsed() && tick.elapsed() < 90 {
+        } else if 75 <= glitch_frames_displayed && glitch_frames_displayed < 125 {
             // the second half of a second, leave the lights on... with a 90% chance of glitching
-            for (i, led) in strip[GLITCH_LED_RANGE].iter_mut().enumerate() {
+            for (i, led) in strip[GLITCH_LED_RANGE_1].iter_mut().enumerate() {
+                let random = rng.gen_range(1..=10);
+                if i % 2 == 0 && random > 9 {
+                    *led = RGB8::default();
+                }
+            }
+            for (i, led) in strip[GLITCH_LED_RANGE_2].iter_mut().enumerate() {
                 let random = rng.gen_range(1..=10);
                 if i % 2 == 0 && random > 9 {
                     *led = RGB8::default();
@@ -80,7 +102,12 @@ fn main() -> ! {
             }
         } else {
             // turn off every other light
-            for (i, led) in strip[GLITCH_LED_RANGE].iter_mut().enumerate() {
+            for (i, led) in strip[GLITCH_LED_RANGE_1].iter_mut().enumerate() {
+                if i % 2 == 0 {
+                    *led = RGB8::default();
+                }
+            }
+            for (i, led) in strip[GLITCH_LED_RANGE_2].iter_mut().enumerate() {
                 if i % 2 == 0 {
                     *led = RGB8::default();
                 }
@@ -88,13 +115,10 @@ fn main() -> ! {
         }
 
         // arrow effect
-        if tick.elapsed() % 10 == 0 {
-            arrow.mutate(&mut strip[ARROW_LED_RANGE]);
-        }
+        arrow.mutate(&mut strip[ARROW_LED_RANGE]);
 
         ws.write(gamma(strip.iter().cloned())).unwrap();
-        arduino_hal::delay_ms(tick.len());
 
-        tick.tock();
+        animation.next();
     }
 }
